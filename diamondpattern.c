556f1f65efab2a0b42c12095fa20136ffa37413a
RushikeshChittalwad <rushikeshchittalwad@gmail.com>
2026-07-19 11:14:07 +0530

    Add diamond pattern printing program in C
    This program prints a diamond pattern based on the number of rows input by the user. It uses nested loops to create the top and bottom halves of the diamond.

---
+++ diamondpattern.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, j, space;
+
+    printf("Enter the number of rows (for the top half): ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            if (j == 1 || j == 2 * i - 1) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    for (i = n - 1; i >= 1; i--) {
+        for (space = 1; space <= n - i; space++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            if (j == 1 || j == 2 * i - 1) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
